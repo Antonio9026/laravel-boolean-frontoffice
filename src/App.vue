469010a9dbf1f423393030e7babd73ac0168b184
acolombo09 <acolombo0911@gmail.com>
2023-10-20 17:12:11 +0200

    adds-header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script>
-export default {
+import TheHeader from "./components/TheHeader.vue"
 
+export default {
+  components: {
+    TheHeader,
+  },
+  data() {
+    return {}
+  }
 }
 </script>
 
 <template>
-<h1>app view</h1>
-<router-view></router-view>
+  <div class="container-fluid px-0">
+    <div class="row">
+      <div class="col">
+        <TheHeader></TheHeader>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<template>
+  <header class="bg-dark">
+    <nav class="container-fluid">
+      <div class="row py-3 d-flex">
+        <div class="col-md-9 d-flex align-items-center justify-content-between mx-auto px-0">
+          <a href="/">
+            <img src="https://riegsecurity.com/wp-content/uploads/2019/09/Riegs-D-Class-Feat.png" alt="header-logo">
+          </a>
+          <ul class="ms-auto d-flex list-unstyled gap-4 mb-0">
+            <li><router-link :to="{name: 'home'}">HOME</router-link></li>
+            <li><router-link :to="{name: 'chi-siamo'}">CHI SIAMO</router-link></li>
+            <li><router-link :to="{name: 'contatti'}">CONTATTI</router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+header img {
+  width: 60px;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+nav a:hover,
+nav a.active {
+  color: #858585;
+}
+
+</style>
