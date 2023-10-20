4a9def53cd41e5864acd43acff705dd66bbb0ecd
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-10-20 15:50:36 +0200

    formatted router

--- src/router.js
+++ src/router.js
@@ -3,30 +3,30 @@ import Home from "./pages/Home.vue";
 import ContattiPage from "./pages/ContattiPage.vue";
 import ChiSiamo from "./pages/ChiSiamo.vue";
 
-const routes = [
-    {
-        path: "/contatti",
-        name: "contatti",
-        component: ContattiPage
-    },
-    {
-        path: "/",
-        name: "home",
-        component: Home
-    },
-    {
-        path: "/chi-siamo",
-        name: "chi-siamo",
-        component: ChiSiamo
-    },
-];
+
 
 
 const router = createRouter({
 
     history: createWebHistory(),
 
-    routes
+    routes: [
+      {
+          path: "/contatti",
+          name: "contatti",
+          component: ContattiPage
+      },
+      {
+          path: "/",
+          name: "home",
+          component: Home
+      },
+      {
+          path: "/chi-siamo",
+          name: "chi-siamo",
+          component: ChiSiamo
+      },
+  ]
 });
 
 
