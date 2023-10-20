c2bd0a2d209f6eae3946fe7477bbe07aebc4aef6
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-20 19:07:58 +0200

    aggiunto alcholic

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,21 +12,19 @@ export default {
 
 <template>
     <div class="card">
-        <div class="card text-center" >
+        <div class="card text-center">
             <img :src="cocktail.thumb" class="card-img-top" alt="">
             <div class="card-body">
                 <h5 class="card-title">{{ cocktail.name }}</h5>
+                <p class="card-text">{{ cocktail.alcoholic }}</p>
                 <p class="card-text">{{ cocktail.category }}</p>
                 <p class="card-text">{{ cocktail.instructions }}</p>
-                <p class="card-text">{{ cocktail.instructions }}</p>
+                
             </div>
-            
+
         </div>
     </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
