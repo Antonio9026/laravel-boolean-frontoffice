4f35bae45751eb1fb668c920edacf1d74ad3b0a9
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-20 15:31:23 +0200

    fix router js

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./pages/Home";
-import ContattiPage from "./pages/ContattiPage";
+import Home from "./pages/Home.vue";
+import ContattiPage from "./pages/ContattiPage.vue";
+import ChiSiamo from "./pages/ChiSiamo.vue";
 const routes = [
     {
-      path: "/", 
-      name: "chi-siamo"
-      component: "ContattiPage",
+      path: "/contatti", 
+      name: "contatti",
+      component: ContattiPage,
     },
     {
         path: "/", 
-        name: "home"
-        component: "Home",
+        name: "home",
+        component: Home,
       },
-],
+      {
+        path: "/", 
+        name: "chi-siamo",
+        component: ChiSiamo,
+      },
+];
 
 
 const router = createRouter({
