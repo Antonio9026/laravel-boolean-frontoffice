5b543594fe8cd23000f96e3cde72ffdd3acfae17
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-20 18:52:29 +0200

    cancellato card footer

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,15 +12,15 @@ export default {
 
 <template>
     <div class="card">
-        <div class="card text-center" style="width: 18rem;">
+        <div class="card text-center" >
             <img :src="cocktail.thumb" class="card-img-top" alt="">
             <div class="card-body">
                 <h5 class="card-title">{{ cocktail.name }}</h5>
                 <p class="card-text">{{ cocktail.category }}</p>
                 <p class="card-text">{{ cocktail.instructions }}</p>
+                <p class="card-text">{{ cocktail.instructions }}</p>
             </div>
-            <div class="card-footer">
-            </div>
+            
         </div>
     </div>
 </template>
