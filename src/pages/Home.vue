bd26130faeb6f770d2f513789b83f9b7e9e0cd10
dariofusco <fdario@hotmail.it>
2023-10-20 16:03:34 +0200

    chiamata axios e recupero dati in html

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,44 @@
 <script>
+import axios from "axios";
 export default {
-
-}
+    data() {
+        return {
+            cocktails: [],
+        }
+    },
+    methods: {
+        fetchData() {
+            axios
+                .get("http://127.0.0.1:8000/api/cocktails")
+                .then((response) => {
+                    this.cocktails = response.data;
+                });
+        },
+    },
+    mounted() {
+        this.fetchData();
+    },
+};
 </script>
 
 <template>
-<h1>Home</h1>
-
+    <h1>Home</h1>
+    <div class="container vh-100">
+        <div class="row">
+            <h1 class="py-3">Lista Cocktails</h1>
+            <div class="col-3" v-for="cocktail in cocktails">
+                <div class="card text-center" style="width: 18rem;">
+                    <img :src="cocktail.thumb" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ cocktail.name }}</h5>
+                        <p class="card-text">{{ cocktail.instructions }}</p>
+                    </div>
+                    <div class="card-footer">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
