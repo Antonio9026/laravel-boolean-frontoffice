ffb039a4f5c44984ee04da5a811e61f816efc9cb
acolombo09 <acolombo0911@gmail.com>
2023-10-23 18:38:39 +0200

    adds-graphics-&-texts

--- src/pages/ChiSiamo.vue
+++ src/pages/ChiSiamo.vue
@@ -6,119 +6,191 @@ export default {
 
 <template>
 
-  <!-- About section The Company
-  <div class="about-section px-4 py-5" style="background-color: #111117;">
-    <div class="col-md-9 mx-auto my-5">
-      <div class="row flex-lg-row justify-content-center align-items-center g-5">
-        <div class="col-md-6">
-          <h5 class="fw-bold text-icons-color lh-1 mb-3">ABOUT THE NETWORK</h5>
-          <h2 class="display-5 fw-bold text-white lh-1 mb-3"><mark class="highlight-text">The</mark> Company</h2>
-          <p class="mt-4 lead text-secondary">For 12 years we have been providing audit and warranty, financial advice, 
-            risk advice, taxes and related services to select clients.
-          </p>
-          
-          <div class="col my-5">
-            <div class="row row-cols-1 row-cols-sm-2 g-4">
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-solid fa-award"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Tradition</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-              </div>
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-solid fa-lock"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Security</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit..</p>
-              </div>
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-regular fa-pen-to-square"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Certificate</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-              </div>
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-solid fa-graduation-cap fa-flip-horizontal"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Expertise</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-              </div>
+  <!-- Hero About Section -->
+  <div class="px-4 py-5 text-center" style="background-color: #f1d9b7;">
+    <h1 class="display-5 fw-bold text-body-emphasis text-center mb-2">Chi Siamo</h1>
+    <div class="col-lg-6 mx-auto text-center mb-4">
+      <div class="d-flex justify-content-center">
+        <hr class="w-25">
+      </div>
+      <h2>Come join us for a lunch, dinner or a drink.</h2>
+    </div>
+
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">
+        Benvenuto nel nostro mondo di cocktail artigianali e mixology innovativa.
+        Se ami l'arte di creare e degustare cocktail unici, sei nel posto giusto. 
+        Presso il nostro cocktail e mixology bar, ci dedichiamo a mescolare ingredienti 
+        di alta qualità con tecniche all'avanguardia per offrirti esperienze di degustazione indimenticabili.
+        Ogni nostro cocktail è una creazione originale, curata nei minimi dettagli per soddisfare i palati più esigenti. 
+        Utilizziamo solo gli ingredienti più freschi e pregiati, selezionati con cura 
+        per garantirti un'esperienza gustativa senza pari. La nostra equipe di barman esperti 
+        è appassionata nel creare nuove e innovative bevande, portando avanti la tradizione 
+        della mixology con un tocco moderno.
+        Presso il nostro locale, non offriamo solo cocktail straordinari, ma anche un'atmosfera unica e accogliente. 
+        Il nostro personale cordiale e professionale è pronto ad accoglierti 
+        e guidarti attraverso un viaggio sensoriale nel mondo dei sapori. 
+        Che tu sia un appassionato di cocktail o un neofita curioso, ti garantiamo un'esperienza 
+        che stimolerà i tuoi sensi e ti lascerà desiderare sempre di più.
+        Vieni a trovarci e scopri il fascino dei nostri cocktail artigianali. 
+        Siamo più di un semplice bar, siamo un destino per gli amanti della mixology e un rifugio 
+        per coloro che cercano emozioni in ogni sorso. 
+        Ti aspettiamo per farti vivere l'arte dei cocktail come mai prima d'ora.
+      </p>
+    </div>
+  </div>
+
+  <!-- Why Us Section -->
+  <div class="container-fluid py-5" style="background-color: #e8cfac;">
+    <h2 class="text-center mb-3"><span>PERCHÈ SCEGLIERE NOI</span></h2>
+    <div class="col-lg-6 mx-auto text-center mb-4">
+      <div class="d-flex justify-content-center">
+        <hr class="w-25">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-9 mx-auto">
+        <div class="row">
+          <div class="col-3">
+            <div>
+              <h3 class="h5 mb-3">MENU VARIEGATO</h3>
+              <p>
+                Nel nostro menu troverai una selezione di cocktail provenienti 
+                da diverse parti del mondo, dalla tradizione italiana e francese 
+                all'innovazione dei mix thailandesi e giapponesi.
+              </p>
             </div>
           </div>
-
-          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" class="btn btn-outline-light text-icons-color border border-icons-color px-4 me-3 custom-button-hover">GET IN TOUCH</button>
-            <button type="button" class="btn btn-outline-light text-icons-color border border-icons-color px-4 custom-button-hover">READ MORE</button>
+          <div class="col-3">
+            <div>
+              <h3 class="h5 mb-3">AMBIENTE ACCOGLIENTE</h3>
+              <p>
+                Al nostro cocktail bar, l'atmosfera è avvolta da soffitti a volta e 
+                ricche texture che creano un ambiente accogliente, permettendo ai 
+                nostri ospiti di sentirsi a proprio agio e immersi nella magia dei drink.
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="col-md-6">
-          
+          <div class="col-3">
+            <div>
+              <h3 class="h5 mb-3">CONSEGNA GRATUITA</h3>
+              <p>
+                Se non hai tempo di venire al nostro cocktail bar, ti portiamo i tuoi 
+                drink preferiti direttamente a casa tua, senza costi aggiuntivi per la consegna.
+              </p>
+            </div>
+          </div>
+          <div class="col-3">
+            <div>
+              <h3 class="h5 mb-3">SERVIZIO ECCELLENTE</h3>
+              <p>
+                La nostra dedizione verso i clienti ci spinge a offrire un servizio di prima classe 
+                per assicurarci che tutti siano soddisfatti.
+                Ci impegniamo al massimo per garantirti un'esperienza indimenticabile 
+                ogni volta che entri nel nostro bar.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
+
+  <!-- Our Team Section -->
+  <div class="container-fluid py-5" style="background-color: #dfbf92;">
+    <div class="row">
+      <div class="col-lg-8 mx-auto text-center">
+        <h2 class="mb-4">Il Nostro Team</h2>
+        <hr class="w-100 mb-4">
+
+        <div class="row mt-5">
+          <div class="col-md-6 mb-4">
+            <img src="/public/lporrega.JPG" alt="lorenzo-porrega" class="img-fluid mb-3">
+            <h3 class="mb-2">Lorenzo Porrega</h3>
+            <p class="text-muted mb-0">PROJECT MANAGER</p>
+            <p class="mb-0">As Executive Chef of our restaurant, she brings extensive knowledge and leadership to the kitchen. She has more than 22 years of experience in the restaurant industry, with the last 14 years as an executive chef.</p>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <img src="/public/apiras.JPG" alt="antonio-piras" class="img-fluid mb-3">
+            <h3 class="mb-2">Antonio Piras</h3>
+            <p class="text-muted mb-0">MIXOLOGIST</p>
+            <p class="mb-0">Committed to teamwork demonstrating integrity and honesty while interacting with guests, team members, and managers. Follows company safety standards at all times.</p>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <img src="/public/ivelikholova.JPG" alt="iryna-velykholova" class="img-fluid mb-3">
+            <h3 class="mb-2">Iryna Velykholova</h3>
+            <p class="text-muted mb-0">INTERIOR DESIGNER & WEB DESIGNER</p>
+            <p class="mb-0">A member of our restaurant's family for more than 15 years, she has worked every position here, giving it a unique management perspective. She has a lifelong passion for the restaurant business.</p>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <img src="/public/dfusco.JPG" alt="dario-fusco" class="img-fluid mb-3">
+            <h3 class="mb-2">Dario Fusco</h3>
+            <p class="text-muted mb-0">SENIOR FULL STACK DEVELOPER</p>
+            <p class="mb-0">He establishes books for food lovers, gives interviews, comes to the most famous TV shows. He is a real celebrity!</p>
+          </div>
+        </div>
+
+        <div class="row mt-2 justify-content-center">
+          <div class="col-md-6 mb-4">
+            <img src="/public/acolombo.JPG" alt="andrea-colombo" class="img-fluid mb-3">
+            <h3 class="mb-2">Andrea Colombo</h3>
+            <p class="text-muted mb-0">MENTAL COACH</p>
+            <p class="mb-0">He establishes books for food lovers, gives interviews, comes to the most famous TV shows. He is a real celebrity!</p>
+          </div>
+        </div>
+        <hr class="w-100 mt-3">
+      </div>
+    </div>
+  </div>
+
 
 </template>
 
 <style lang="scss" scoped>
 
-// .about-section {
-//   background: 
-//     linear-gradient(to left, transparent 50%, #111117 50%), 
-//     url(../assets/images/about-4.jpg) no-repeat right center; 
-//   object-fit: contain;
-//   min-height: 100vh; 
-//   background-position: center right;
-//   position: relative;
-// }
+.about-section {
+  background:
+    linear-gradient(to left, transparent 50%, #dfc39d 50%),
+    url(../assets/images/about-4.jpg) no-repeat right center;
+  object-fit: contain;
+  min-height: 100vh;
+  background-position: center right;
+  position: relative;
+}
 
-// .text-icons-color {
-//   color: green !important;
-// }
+.text-icons-color {
+  color: green !important;
+}
 
-// .header-footer-color {
-//   color: green !important;
-// }
+.header-footer-color {
+  color: green !important;
+}
 
-// .border-icons-color {
-//   border-color: green !important;
-// }
+.border-icons-color {
+  border-color: green !important;
+}
 
-// .custom-button {
-//   background-color: green !important;
-//   color: white !important;
-//   border: none !important;
-// }
+.custom-button {
+  background-color: green !important;
+  color: white !important;
+  border: none !important;
+}
 
-// .custom-button-hover:hover {
-//   background-color: green !important;
-//   color: white !important;
-//   border: none !important;
-// }
+.custom-button-hover:hover {
+  background-color: green !important;
+  color: white !important;
+  border: none !important;
+}
 
-// .highlight-text {
-//   background-color: rgba(#00A6A6, 02); 
-//   color: inherit; 
-// }
+.top {
+  top: -27px;
+}
 
-// .top {
-//   top: -27px;
-// }
-
-// .bottom {
-//   bottom: -72px;
-// }
+.bottom {
+  bottom: -72px;
+}
 
 </style>
