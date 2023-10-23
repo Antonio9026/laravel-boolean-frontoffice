6e8d173e7c01715f2eaeb90a401e4db0b5fdeba1
acolombo09 <acolombo0911@gmail.com>
2023-10-23 22:29:25 +0200

    ending-tag-added

--- src/pages/ChiSiamo.vue
+++ src/pages/ChiSiamo.vue
@@ -8,63 +8,6 @@ export default {
 <div class="container mt-0">
 
 
-<<<<<<< HEAD
-  <!-- About section The Company
-  <div class="about-section px-4 py-5" style="background-color: #111117;">
-    <div class="col-md-9 mx-auto my-5">
-      <div class="row flex-lg-row justify-content-center align-items-center g-5">
-        <div class="col-md-6">
-          <h5 class="fw-bold text-icons-color lh-1 mb-3">ABOUT THE NETWORK</h5>
-          <h2 class="display-5 fw-bold text-white lh-1 mb-3"><mark class="highlight-text">The</mark> Company</h2>
-          <p class="mt-4 lead text-secondary">For 12 years we have been providing audit and warranty, financial advice, 
-            risk advice, taxes and related services to select clients.
-          </p>
-          
-          <div class="col my-5">
-            <div class="row row-cols-1 row-cols-sm-2 g-4">
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-solid fa-award"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Tradition</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-              </div>
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-solid fa-lock"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Security</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit..</p>
-              </div>
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-regular fa-pen-to-square"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Certificate</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-              </div>
-              <div class="col d-flex flex-column gap-2">
-                <div class="d-flex align-items-center">
-                  <div class="d-inline-flex align-items-center justify-content-center me-2 fs-5 rounded-3 text-icons-color">
-                    <i class="fa-solid fa-graduation-cap fa-flip-horizontal"></i>
-                  </div>
-                  <h4 class="fw-semibold mb-0 text-white">Expertise</h4>
-                </div>
-                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-              </div>
-            </div> 
-          </div>-->
-          <h1>Blank test - WIP</h1>
-          <!-- <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" class="btn btn-outline-light text-icons-color border border-icons-color px-4 me-3 custom-button-hover">GET IN TOUCH</button>
-            <button type="button" class="btn btn-outline-light text-icons-color border border-icons-color px-4 custom-button-hover">READ MORE</button>
-=======
   <!-- Hero About Section -->
   <div class="px-4 py-5 text-center" style="background-color: #dfbf92;">
     <h1 class="display-5 fw-bold text-body-emphasis text-center mb-2">Chi Siamo</h1>
@@ -172,15 +115,10 @@ export default {
                 ogni volta che entri nel nostro bar.
               </p>
             </div>
->>>>>>> chi-siamo-page
           </div>
         </div>
       </div>
     </div>
-<<<<<<< HEAD
-  </div> -->
-</div>
-=======
   </div>
 
   <!-- Our Team Section -->
@@ -250,28 +188,12 @@ export default {
       </div>
     </div>
   </div>
-
->>>>>>> chi-siamo-page
+</div>
 
 </template>
 
 <style lang="scss" scoped>
 
-<<<<<<< HEAD
-.container{
-    margin-top: 40px;
-    min-height: calc(100vh - 214px);
-}
-// .about-section {
-//   background: 
-//     linear-gradient(to left, transparent 50%, #111117 50%), 
-//     url(../assets/images/about-4.jpg) no-repeat right center; 
-//   object-fit: contain;
-//   min-height: 100vh; 
-//   background-position: center right;
-//   position: relative;
-// }
-=======
 .about-section {
   background:
     linear-gradient(to left, transparent 50%, #dfc39d 50%),
@@ -281,7 +203,6 @@ export default {
   background-position: center right;
   position: relative;
 }
->>>>>>> chi-siamo-page
 
 .text-icons-color {
   color: green !important;
